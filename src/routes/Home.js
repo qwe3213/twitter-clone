47f59146f17840b846f400c7ab8c9b4a6da56d11
hayoon <hs183000@naver.com>
2022-07-25 22:42:57 +0900

    Nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,23 @@
-import{collection,addDoc} from "firebase/firestore"
-import React,{useState} from "react";
+import{collection,addDoc,getDocs,query} from "firebase/firestore"
+import React,{useEffect, useState} from "react";
 import {dbService} from 'fbase.js';
 const Home =()=> {
-    const [nweet,setNweet]=useState("")
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  const getNweets = async () => {
+    const q = query(collection(dbService, "nweets"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+    const nweetObj = {
+    ...doc.data(),
+    id: doc.id,
+    }
+    setNweets(prev => [nweetObj, ...prev]);
+    });
+    };
+    useEffect(() => {
+    getNweets();
+    }, []);
     const onSubmit= async(event)=>{
       event.preventDefault()
       await addDoc(collection(dbService, "nweets"), {
@@ -17,12 +32,21 @@ const Home =()=> {
       } =event;
       setNweet(value)
     }
+    console.log(nweets);
     return( 
         <div>
             <form onSubmit={onSubmit}>
                 <input value={nweet} onChange={onChange} type="text" placeholder="What's  on your mind?" maxLength={120} />
                 <input type="submit" value="Nweet" />
             </form>
+            <div>
+              {nweets.map((nweet=>
+                <div ket={nweet}>
+                  <h4>{nweet.nweet}</h4>
+                </div>
+
+              ))}
+            </div>
         </div>
         )
     
